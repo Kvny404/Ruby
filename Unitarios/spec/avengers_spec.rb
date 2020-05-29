2d393ae3e5a6dee6b095c3400f2a8b05c8a5b4d2
Kvny404 <kvny.martins@gmail.com>
2020-05-28 23:48:43 -0300

    Checkers and Matchers

--- Unitarios/spec/avengers_spec.rb
+++ Unitarios/spec/avengers_spec.rb
@@ -19,4 +19,41 @@ describe AvengersHeadQuarter do
         hq.put('Spiderman')
         expect(hq.list).to eql ['Spiderman']
     end
+
+    it 'deve adicionar uma lista de vingadores' do    
+        hq  = AvengersHeadQuarter.new
+        hq.put('Thor')
+        hq.put('Hulk')
+        hq.put('Spiderman')
+
+        # res = hq.list.size > 0
+        # expect(res).to be true
+        expect(hq.list.size).to be > 0
+    end
+
+    it 'Thor deve se o primeiro da lista' do
+        hq  = AvengersHeadQuarter.new
+        hq.put('Thor')
+        hq.put('Hulk')
+        hq.put('Spiderman')
+
+        expect(hq.list).to start_with('Thor')
+    end
+
+    it 'Ironman deve se o ultimo da lista' do
+        hq  = AvengersHeadQuarter.new
+        hq.put('Thor')
+        hq.put('Hulk')
+        hq.put('Spiderman')
+        hq.put('Ironman')
+
+        expect(hq.list).to end_with('Ironman')
+    end
+
+    it 'deve conter o sobrenome' do
+        avenger = 'Peter Parker'
+
+        expect(avenger).to match(/Parker/)
+        expect(avenger).to not_match(/Keveny/)
+    end 
 end
