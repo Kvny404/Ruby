9fca3fb4b1e79e5a2f42af2ddefabc6a6cc4eae7
Kvny404 <kvny.martins@gmail.com>
2020-06-08 22:05:52 -0300

    Added Notification String Check

--- Capy/spec/forms_spec.rb
+++ Capy/spec/forms_spec.rb
@@ -10,7 +10,10 @@ describe 'Forms' do
         click_button 'Login' # Clica no elemento selecionado
 
         expect(find('#flash').visible?).to be true # Procura pelo elemento e verifica se está visível
-
+    
+        expect(find('#flash').text).to include 'Olá, Tony Stark. Você acessou a área logada!' # Checa se uma String contém outra
+        
+        expect(find('#flash')).to have_content 'Olá, Tony Stark. Você acessou a área logada!' # Checa se o elemento contém a String
     end
 
 end
